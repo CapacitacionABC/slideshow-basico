3c69e85418e309cc57e587fa61a9e6e7fc3ca0f3
Alfredo Bravo Cuero <abravo@iterasoft.co>
2018-08-05 18:46:22 -0500

    creacion de slide basico con GSAP

---
+++ js/slideshow.js
@@ -0,0 +1,57 @@
+(function(){
+
+	var actual = 0;
+	var ancho = 600;
+
+
+	var $slideshow = $(".slideShow ul");
+	var slide = $slideshow.find("li").length;
+
+	// correr automaticamente los slide cada intervalo de tiempo 
+	var intervalo = setInterval(function(){
+		mover("sig");
+	}, 1500 );
+
+
+	function mover( dir, click ){
+
+		if( click )
+			clearInterval( intervalo );
+
+		( dir === "sig" ) ? actual-- : actual++; 
+
+		if( actual > 0){
+			actual = ( slide -1) * -1;
+		}else if ( actual <= ( slide * -1 ) ){
+			actual = 0
+		}
+
+		console.log( actual );
+		
+
+		var margen = actual * ancho;
+
+		// Animacion con GSAP
+		var tl = new TimelineMax();
+
+		tl.to( $slideshow, 1, {
+			marginLeft: margen
+		});
+
+		// Animacion con Animate
+		// $slideshow.animate({
+		// 	marginLeft: margen
+		// }, 400);
+		
+
+
+	}
+
+	$(".botSlide").on("click", function(){
+
+		var dir = $(this).data("mov");
+
+		mover( dir, true );
+	});
+
+}());
