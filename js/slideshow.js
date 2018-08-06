15f77e425cee12e10b2fa50f35211d97a130ef24
Alfredo Bravo Cuero <abravo@iterasoft.co>
2018-08-05 21:21:21 -0500

    slide finalizado se agregan puntos de control de transicion

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,6 +6,11 @@
 
 	var $slideshow = $(".slideShow ul");
 	var slide = $slideshow.find("li").length;
+	var $puntos = $(".slideShowButtons");
+
+	$puntos.find("div").eq(0).css({
+		backgroundColor: "#58167d"
+	})
 
 	// correr automaticamente los slide cada intervalo de tiempo 
 	var intervalo = setInterval(function(){
@@ -15,9 +20,6 @@
 
 	function mover( dir, click ){
 
-		if( click )
-			clearInterval( intervalo );
-
 		( dir === "sig" ) ? actual-- : actual++; 
 
 		if( actual > 0){
@@ -25,11 +27,21 @@
 		}else if ( actual <= ( slide * -1 ) ){
 			actual = 0
 		}
-
-		console.log( actual );
 		
+		mover_por_punto( actual, click );
+	}
+
+
+	function mover_por_punto( actual, click ){
+
+		if( click )
+			clearInterval( intervalo );
+
 
 		var margen = actual * ancho;
+		var idx = actual * -1;
+		var $puntoActual = $puntos.find("div").eq( idx );
+		var $demasPuntos = $puntos.find("div").not( $puntoActual );
 
 		// Animacion con GSAP
 		var tl = new TimelineMax();
@@ -37,16 +49,25 @@
 		tl.to( $slideshow, 1, {
 			marginLeft: margen,
 			ease: Circ.easeOut
-		});
+		}).to( $puntoActual, 0.5, { backgroundColor: "#58167d" }, "-=1.2" )
+		  .to( $demasPuntos, 0.5, { backgroundColor: "#a1a1a1" }, "-=1.2" );
 
 		// Animacion con Animate
 		// $slideshow.animate({
 		// 	marginLeft: margen
 		// }, 400);
-		
-
 
 	}
+
+	$(".slideButton").on("click", function(){
+
+		var idx = $(this).data("idx");
+		idx = idx * -1;
+		mover_por_punto( idx, true );
+		
+		
+	})
+
 
 	$(".botSlide").on("click", function(){
 
