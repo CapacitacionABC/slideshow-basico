7f0f5db4b44cb591949e871962b1ee19c05ef4be
Alfredo Bravo Cuero <abravo@iterasoft.co>
2018-08-05 22:20:26 -0500

    slideShow convertido a plugin

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,79 +1,125 @@
 (function(){
 
-	var actual = 0;
-	var ancho = 600;
+	$.slideshow = function( opciones ){
+
+		opciones = $.extend({
+			divDestino: ".slideShow",
+			intervalo: 1500,
+			ancho: 600,
+			velocidad: 1200,
+			slides: [],
+			color: "#ba007c"
+		}, opciones );
+
+		if( opciones.slides.length == 0 ){
+			alert("Los slides son necesarios");
+			return;
+		}
+
+		opciones.velocidad = opciones.velocidad / 1000;
+
+		var actual = 0;
+		var ancho = opciones.ancho;
 
 
-	var $slideshow = $(".slideShow ul");
-	var slide = $slideshow.find("li").length;
-	var $puntos = $(".slideShowButtons");
+		var slide = opciones.slides.length;
 
-	$puntos.find("div").eq(0).css({
-		backgroundColor: "#58167d"
-	})
+		// creacion de slides
+		var contenido = "";
 
-	// correr automaticamente los slide cada intervalo de tiempo 
-	var intervalo = setInterval(function(){
-		mover("sig");
-	}, 1500 );
+			contenido += "<ul>"
+				for (var i = 0; i < opciones.slides.length; i++ ) {
+					contenido += '<li><img src="'+ opciones.slides[i] +'"></li>';
+				}
+			contenido += "</ul>"; 
 
+		$( opciones.divDestino ).append( contenido );
 
-	function mover( dir, click ){
+		var $slideshow = $( opciones.divDestino + " ul");
 
-		( dir === "sig" ) ? actual-- : actual++; 
+		// Creacion de los botones
 
-		if( actual > 0){
-			actual = ( slide -1) * -1;
-		}else if ( actual <= ( slide * -1 ) ){
-			actual = 0
+		contenido = "";
+		contenido += '<div class="slideShowButtons">';
+		for (var i = 0; i < opciones.slides.length; i++ ) {
+			contenido += '<div data-idx="'+ i +'" class="slideButton"></div>';
 		}
 		
-		mover_por_punto( actual, click );
-	}
+		contenido += '<div';
+
+		$( opciones.divDestino ).append( contenido );
 
 
-	function mover_por_punto( actual, click ){
+		var $puntos = $(".slideShowButtons");
 
-		if( click )
-			clearInterval( intervalo );
+		$puntos.find("div").eq(0).css({
+			backgroundColor: opciones.color
+		})
 
-
-		var margen = actual * ancho;
-		var idx = actual * -1;
-		var $puntoActual = $puntos.find("div").eq( idx );
-		var $demasPuntos = $puntos.find("div").not( $puntoActual );
-
-		// Animacion con GSAP
-		var tl = new TimelineMax();
-
-		tl.to( $slideshow, 1, {
-			marginLeft: margen,
-			ease: Circ.easeOut
-		}).to( $puntoActual, 0.5, { backgroundColor: "#58167d" }, "-=1.2" )
-		  .to( $demasPuntos, 0.5, { backgroundColor: "#a1a1a1" }, "-=1.2" );
-
-		// Animacion con Animate
-		// $slideshow.animate({
-		// 	marginLeft: margen
-		// }, 400);
-
-	}
-
-	$(".slideButton").on("click", function(){
-
-		var idx = $(this).data("idx");
-		idx = idx * -1;
-		mover_por_punto( idx, true );
-		
-		
-	})
+		// correr automaticamente los slide cada intervalo de tiempo 
+		var intervalo = setInterval(function(){
+			mover("sig");
+		}, opciones.intervalo );
 
 
-	$(".botSlide").on("click", function(){
+		function mover( dir, click ){
 
-		var dir = $(this).data("mov");
+			( dir === "sig" ) ? actual-- : actual++; 
 
-		mover( dir, true );
-	});
+			if( actual > 0){
+				actual = ( slide -1) * -1;
+			}else if ( actual <= ( slide * -1 ) ){
+				actual = 0
+			}
+			
+			mover_por_punto( actual, click );
+		}
+
+
+		function mover_por_punto( actual, click ){
+
+			if( click )
+				clearInterval( intervalo );
+
+
+			var margen = actual * ancho;
+			var idx = actual * -1;
+			var $puntoActual = $puntos.find("div").eq( idx );
+			var $demasPuntos = $puntos.find("div").not( $puntoActual );
+
+			// Animacion con GSAP
+			var tl = new TimelineMax();
+
+			tl.to( $slideshow, opciones.velocidad, {
+				marginLeft: margen,
+				ease: Circ.easeOut
+			}).to( $puntoActual, 0.5, { backgroundColor: opciones.color }, "-=1.2" )
+			  .to( $demasPuntos, 0.5, { backgroundColor: "#a1a1a1" }, "-=1.2" );
+
+			// Animacion con Animate
+			// $slideshow.animate({
+			// 	marginLeft: margen
+			// }, 400);
+
+		}
+
+		$(".slideButton").on("click", function(){
+
+			var idx = $(this).data("idx");
+			idx = idx * -1;
+			mover_por_punto( idx, true );
+			
+			
+		})
+
+		$(".botSlide").on("click", function(){
+
+			var dir = $(this).data("mov");
+
+			mover( dir, true );
+		});
+
+
+	};
 
 }());
